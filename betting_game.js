ff6db4d7324703e49b7027c9857da95405ac40f0
yemelyn <pommy.shortcake@gmail.com>
2016-07-25 18:56:16 +0000

    first commit

---
+++ betting_game.js
@@ -0,0 +1,22 @@
+for (var bankroll = 100; bankroll > 0; bankroll){
+  var bet = Math.floor(prompt("Would you like to bet $5 or $10?"));
+  var number = Math.floor(prompt("Great, now pick a number between 1 and 10"));
+  console.log(number)
+  var compNumber = Math.floor(Math.random() * 2) + 1;
+  console.log(compNumber);
+  switch(number) {
+    case compNumber:
+        bankroll += bet;
+        alert(`Congratulations you won! You now have $${bankroll}.`)
+        break;
+    case compNumber + 1:
+        alert(`Close you were off by one! You now have $${bankroll}.`)
+        break;
+    case compNumber - 1:
+        alert(`Close you were off by one! You now have $${bankroll}.`)
+        break;
+    default:
+        bankroll -= bet;
+        alert(`Sorry you didn't win this time! You now have $${bankroll}.`)
+  }
+}
