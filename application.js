e90782a064392983f6bed31acfd8b3617ba144c3
yemelyn <pommy.shortcake@gmail.com>
2016-07-26 01:34:15 +0000

    finished the prompt box

--- application.js
+++ application.js
@@ -1,31 +1,48 @@
 $(document).ready(function(){
-  var bankroll = 100;
+  var bankroll = 100
+  $('p').text(bankroll)
   $('form').on('submit', function(event){
     event.preventDefault();
-    var bet = +$(this).closest('form').find('#bet').val();
-    var number = +$(this).closest('form').find('#number').val();
-    var compNumber = Math.floor(Math.random() * 10) + 1;
-    console.log(compNumber);
-    console.log(number);
-    switch(Math.abs(compNumber - number)) {
-    case 0:
-      bankroll += bet;
-      $(this).closest('body').find('p').text('Congratulations you won! \n Your guess: '+number+'\nNumber: '+compNumber+'\nYou now have $'+bankroll);
-      break;
-    case 1:
-      $(this).closest('body').find('p').text('Close you were off by one! \n Your guess: '+number+'\nNumber: '+compNumber+'\nYou now have $'+bankroll);
-      break;
-    default:
-      bankroll -= bet;
-      $(this).closest('body').find('p').text('Sorry you did not win this time! \n Your guess: '+number+'\nNumber: '+compNumber+'\nYou now have $'+bankroll);
-    }
-
-  })
-
-
+      if (bankroll == 0) {
+        if (confirm("Would you like to play again?")){
+          bankroll = 100;
+        };
+      }
+      var bet = +$(this).closest('form').find('#bet').val();
+      if (bet < 5 || bet > 10) {
+        return handleError('Please bet between $5 and $10');
+      }
+      var number = +$(this).closest('form').find('#number').val();
+      if (number < 1 || number > 10) {
+        return handleError('Please choose a number between $5 and $10');
+      }
+      var compNumber = Math.floor(Math.random() * 10) + 1;
+      if (bet > bankroll) {
+        alert("You cannot bet more than you have");
+        return false
+      };
+      if (number == "" || bet == "") {
+        alert("Please input fields");
+        return false
+      };
+      console.log(compNumber);
+      console.log(number);
+      switch(Math.abs(compNumber - number)) {
+        case 0:
+          bankroll += bet;
+          $(this).closest('body').find('p').text('Congratulations you won! \n Your guess: '+number+'\nNumber: '+compNumber+'\nYou now have $'+bankroll);
+          break;
+        case 1:
+          $(this).closest('body').find('p').text('Close you were off by one! \n Your guess: '+number+'\nNumber: '+compNumber+'\nYou now have $'+bankroll);
+          break;
+        default:
+          bankroll -= bet;
+          $(this).closest('body').find('p').text('Sorry you did not win this time! \n Your guess: '+number+'\nNumber: '+compNumber+'\nYou now have $'+bankroll);
+      };
+  });
+  function handleError(message){
+    alert(message);
+    return false;
+  };
 })
-
-
-
-
 
